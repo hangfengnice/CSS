b3ca25fa2dd916be131311775a1e6a7480fd9bce
hangfeng <nicehangfeng@foxmail.com>
2020-05-31 22:25:54 +0800

    fix: relearn css3

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
-// import './scss/index.scss'
+import './scss/index.scss'
 
-import './scss/triangle.css'
+// import './scss/triangle.css'
+
+
+    let delay = 0.3;
+    let revealText = document.querySelector('.reveal')
+    let letters = revealText.textContent.split('')
+    revealText.textContent = ''
+    let middle = letters.filter(e => e !== ' ').length / 2
+    console.log(middle);
+    letters.forEach((letter,i) => {
+      let span = document.createElement('span')
+      span.textContent = letter
+      span.style.animationDelay = `${delay + Math.abs(i - middle) * .1}s`
+      revealText.append(span)
+    })
